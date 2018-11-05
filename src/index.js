2f1f6ddd30023ee20dcac9f4a3228c670abf8b21
marycodes2 <mbeliveau92@gmail.com>
2018-11-05 10:45:02 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+// const fullname = document.getElementById("fullname");
+// console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("CONTENT LOADED!");
+// });
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
-});
+// function fetchData(){
+//         var data = fetch('https://randomuser.me/api/')
+//           .then( res => res.json() )
+//           .then( res => res )
+//     console.log(data)
+// }
+//
+// fetchData()
+
+// function fetchData(){
+//         var data = fetch('https://randomuser.me/api/')
+//           .then( res => res.json() )
+//           .then( json => console.log(json) )
+// }
+//
+// fetchData()
+
+function fetchData(){
+        var data = fetch('https://randomuser.me/api/')
+          .then( res => res.json() )
+          .then( console.log )
+}
+
+fetchData()
